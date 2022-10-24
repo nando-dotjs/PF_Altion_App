240a1c259477340a3f205f29d906078b3619a209
Fernando <fperez.personal@gmail.com>
2022-10-23 21:43:05 -0300

    WebView

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      style={styles.container}
+      source={{ uri: 'http://192.168.1.11:3000/' }}
+    />
   );
 }
 
