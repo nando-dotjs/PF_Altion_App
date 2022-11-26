327f3fd0fc0360635c92b379d7a8d5d69b2c59e8
Fernando <fperez.personal@gmail.com>
2022-11-26 02:35:21 -0300

    Implementación de ícono, nombre y splash screen

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { StyleSheet} from 'react-native';
 import { WebView } from 'react-native-webview';
+import * as SplashScreen from 'expo-splash-screen';
+
+// Prevent native splash screen from autohiding before App component declaration
+SplashScreen.preventAutoHideAsync()
+  .then(result => console.log(`SplashScreen.preventAutoHideAsync() succeeded: ${result}`))
+  .catch(console.warn); // it's good to explicitly catch and inspect any error
 
 export default function App() {
+
+  setTimeout(async () => {
+    await SplashScreen.hideAsync();
+  }, 3000);
+
   return (
     <WebView
       style={styles.container}
